b05f4b2e84b1523da3980c46186572679c98bd39
Ivan Denisenko <iamrealvi@gmail.com>
2026-02-27 11:54:41 +0700

    feat: инициализация проекта, базовая настройка алиасов и линтеров

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { TasksPage } from 'pages/tasks';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TasksPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/tasks/tasksPage.tsx
@@ -0,0 +1,7 @@
+export const TasksPage = () => {
+  return (
+    <>
+      <p>TasksPage</p>
+    </>
+  );
+};
